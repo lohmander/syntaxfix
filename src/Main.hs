556e5cbb581b43c12e3c1f5f45c88d8d64429c75
Hannes Lohmander <hannes@lohmander.me>
2017-03-05 14:18:07 +0100

    Rename cli

--- src/Main.hs
+++ src/Main.hs
@@ -18,13 +18,13 @@ cmdParse []     = getContents
 cmdParse fs     = concat `fmap` mapM readFile fs
 
 
-usage   = putStrLn "Usage: haru [-vh] [file...]"
-version = putStrLn "Haru 0.0"
+usage   = putStrLn "Usage: syntaxfix [-vh] [file...]"
+version = putStrLn "Syntaxfix 0.0"
 exit    = exitWith ExitSuccess
 die     = exitWith (ExitFailure 1)
 
 
 gen :: String -> String
-gen c = case (parse parser "./test.hu" c) of
+gen c = case (parse parser "./test.sf" c) of
     Left err -> parseErrorPretty err
     Right st -> JS.print $ JS.transform st
